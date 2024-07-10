d173af97b2da7f50fee8a1ac0bcfdd69c7395be7
Kai Steltner <steltner.kai@gmail.com>
2024-07-10 08:12:11 +0200

    feat: update example for mmfosm

--- examples/airbeam_vol/config_rdo.py
+++ examples/airbeam_vol/config_rdo.py
@@ -3,12 +3,16 @@
 
 number_of_rv = 2
 
-mean_rv = [0, 0]
-sigma_rv = [-5, -6]
-delta_rv = [-7.5, -9]
+mean_rv = [[0, 2], [-1.1284, 2], [1.1284, 2]]
+sigma_rv = [[0.60281, 0.4], [0.60281, 0.4], [0.60281, 0.4]]
+delta_rv = [[0.5, 0.6], [0.5, 0.6], [0.5, 0.6]]
 
 use_central_differences = False
 kappa = 3
+
+# Flag and weights for MMFOSM
+mmfosm = True
+weights = [0.33, 0.33, 0.33]
 
 # Set to true if running on windows machine, false if running on linux
 
